76efcbeb23260846b65b6043f8cf210ac35c8efb
gray_sheep <crazy_egg2009@qq.com>
2018-03-16 15:19:49 +0800

    add demo page

--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,8 @@ var webpack = require('webpack')
 module.exports = {
   entry: './demo/index.js',
   output: {
-    path: path.resolve(__dirname, './dist'),
-    publicPath: '/dist/',
+    path: path.resolve(__dirname, './static'),
+    publicPath: '/static/',
     filename: 'build.js'
   },
   module: {
